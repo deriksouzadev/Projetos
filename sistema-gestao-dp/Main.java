d21b0d7a8e0ce5851513fe2e9671334ef606bf9e
Dérik de Souza dos Santos <111590881+deriksouzadev@users.noreply.github.com>
2026-08-10 16:32:17 -0300

    feat: adiciona estrutura inicial do sistema

---
+++ sistema-gestao-dp/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" SISTEMA DE GESTÃO DE DP");
+        System.out.println("================================");
+
+    }
+}
